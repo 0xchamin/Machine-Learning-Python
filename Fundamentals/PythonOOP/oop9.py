36099ebff683d4ae2af8fda636fcdec885157b29
chaminn <chmk90@gmail.com>
2018-10-31 20:27:37 +0800

    complete Python OOP initial phase"

--- Fundamentals/PythonOOP/oop9.py
+++ Fundamentals/PythonOOP/oop9.py
@@ -13,7 +13,7 @@ class Employee:
         #number should be same across all objects
         Employee.num_of_employees += 1
 
-    def getFullName(self):
+    def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
@@ -35,6 +35,22 @@ class Employee:
         else:
             return True
 
+    ##representation of object
+    ## debugging logging
+    def __repr__(self):
+        ##trying to return a string that can use to re-create object
+        return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+
+    ##readable represenation of an object
+    ## for end users
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)
+
+    def __add__(self, other):
+        return self.pay + other.pay
+
+    def __len__(self):
+        return len(self.fullname())
 
 class Developer(Employee):
     raise_amount = 1.10
@@ -67,6 +83,12 @@ class Manager(Employee):
     #pass
 
 print(Employee.num_of_employees)
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'Employee', 60000)
+
+
+
 dev_1 = Developer("Chamin", "Nalinda", 5000, 'Python')
 dev_2 = Developer("Thilina", "Gamalath", 6000, 'Java')
 dev_3 = Developer("Saman", "Alvitigla", 5200, 'Lisp')
@@ -74,12 +96,25 @@ dev_3 = Developer("Saman", "Alvitigla", 5200, 'Lisp')
 
 mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
 
-print(isinstance(mgr_1, Developer))
-print(isinstance(mgr_1, Employee))
+print(1+2)
+print('a'+'b')
+
+print("\n \n")
+
+print(emp_1)
+
+print(repr(emp_1))
+print(str(emp_1))
+
+print("\n \n")
+print(int.__add__(1,3))
+print(str.__add__('Chamin', ' Nalinda'))
+
+print(emp_1 + emp_2)
 
 
-print(issubclass(Manager, Employee))
-print(issubclass(Developer, Employee))
-print(issubclass(Manager, Developer))
+print(len('test'))
+print('test'.__len__())
 
-print(dev_1)
+
+print(len(emp_1))
