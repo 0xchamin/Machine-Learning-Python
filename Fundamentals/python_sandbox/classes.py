29d867700656ff920d9f6d70616319320b959681
chaminn <chmk90@gmail.com>
2018-11-28 12:29:52 +0800

    finished python fundamental crash course

--- Fundamentals/python_sandbox/classes.py
+++ Fundamentals/python_sandbox/classes.py
@@ -1,2 +1,53 @@
 # A class is like a blueprint for creating objects. An object has properties and methods(functions) associated with it. Almost everything in Python is an object
 
+class User:
+  #constrcutor
+  def __init__(self, name, email, age):
+    self.name = name 
+    self.email = email
+    self.age = age 
+
+  def greetings(self):
+    return f'My name is {self.name} and I am {self.age}'
+
+  def has_birthday(self):
+    self.age +=1
+
+#init user object 
+brad = User('Brad Traversy', 'brad@gmail.com', 37)
+
+#edit property 
+brad.age = 38
+
+print(brad.name)
+
+print(brad.age)
+
+print(brad.greetings())
+
+print(brad.has_birthday())
+
+print(brad.greetings())
+
+  
+class Customer(User):
+  def __init__(self, name, email, age):
+    self.name = name
+    self.email = email 
+    self.age = age 
+    self.balance = 0
+
+  def set_balance(self, balance):
+    self.balance = balance 
+
+  def greetings(self):
+    return f'My name is {self.name} and I am {self.age} and I owe a balance of {self.balance}'
+
+#Init customer 
+cust1 = Customer('Joh Doe', 'john@gmail.com', 40)
+
+print(cust1.name)
+
+cust1.set_balance(500)
+
+print(cust1.greetings())
--- Fundamentals/python_sandbox/modules.py
+++ Fundamentals/python_sandbox/modules.py
@@ -1,1 +1,24 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+
+import datetime
+from datetime import date 
+
+import time
+
+#import file from location 
+import validator 
+
+#today = datetime.date.today() 
+
+today = date.today() 
+print(today)
+
+timestamp = time.time()
+print(timestamp)
+
+email = 'abcdef@gmail.com'
+
+if validator.validate_email(email):
+  print('Valid')
+else:
+  print('Not valid')
